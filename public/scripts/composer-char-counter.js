47ba233dab5461f7e68a5c3507ab3e79b455b6de
arslanah99 <Arah09@hotmail.com>
2019-02-20 04:21:52 +0000

    fixed and colored counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,13 +1,20 @@
 $(document).ready(function() {
-    // --- our code goes here ---
-$('textarea').keypress(function() {
-   var inputBoxValue = ($('textarea').val().length)
-   inputBoxValue = inputBoxValue + 1
-   var spanNumber = $('.counter').val()
-   var afterNum = Number(spanNumber)
-   console.log(inputBoxValue)
-   console.log(afterNum)
-})
-    // $('.counter').html(counter_value)
+  // --- our code goes here ---
+  $("textarea").keydown(function() {
+    var inputBoxValuesLength = $("textarea").val().length;
+    var hardCodedCounterValue = 140;
+    var subtractInputAndHardCodeValues = hardCodedCounterValue - inputBoxValuesLength;
+    $(this)
+      .siblings(".counter")
+      .text(subtractInputAndHardCodeValues);
+    if (subtractInputAndHardCodeValues <= 0) {
+      $(this)
+        .siblings(".counter")
+        .css({ color: "red" });
+    } else{
+        $(this)
+        .siblings(".counter")
+        .css({ color: "gray" })
+    }
   });
-  
+});
