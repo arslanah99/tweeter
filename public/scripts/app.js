e600d1ace32fa26e1fb8359d7f46596f56b2b7a2
arslanah99 <Arah09@hotmail.com>
2019-02-22 04:14:19 +0000

    completed compose

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -7,60 +7,39 @@
   /* <script src="https://code.jquery.com/jquery-1.10.2.js"></script> */
 }
 
-
-
-
 $(document).ready(function() {
-
-
-
-
-
-
-
-function loadTweets(){
-  $.ajax('/tweets', {method: 'GET'})
-    .then(function(tweets){
-      renderTweets(tweets)
-    })
-}
-
-
-
-
+  $(".composebutton").click(function() {
+    $(".new-tweet").slideToggle("slow");
+    $("#TweetBox").focus();
+  });
+  function loadTweets() {
+    $.ajax("/tweets", { method: "GET" }).then(function(tweets) {
+      renderTweets(tweets);
+    });
+  }
   function renderTweets(tweets) {
     tweets.forEach(function(tweet) {
       var tweetElement = createTweetElement(tweet);
       $("#texts").prepend(tweetElement);
     });
   }
-
-
-  $('form').submit(function(event){
-    event.preventDefault()
-    var contentLength = $('#TweetBox').val();
-    var counting = $('.counter').val();
-    console.log(contentLength.length)
-  
-    if(!contentLength){
-      console.log(contentLength)
-      alert('Not Allowed you must enter valid string')
-    } else if(contentLength.length >= 140){
-      alert('Too many characters')
+  $("form").submit(function(event) {
+    event.preventDefault();
+    var contentLength = $("#TweetBox").val();
+    if (!contentLength) {
+      console.log(contentLength);
+      alert("Not Allowed you must enter valid string");
+    } else if (contentLength.length >= 140) {
+      alert("Too many characters");
     } else {
-      $.ajax('/tweets', {
-        method: 'POST',
-        data: $('form').serialize(),
+      $.ajax("/tweets", {
+        method: "POST",
+        data: $("form").serialize(),
         complete: loadTweets
-        // error: errorChecker
-  
-      })
-      $('#TweetBox').val('')
-  
+      });
+      $("#TweetBox").val("");
     }
-    
-  
-  })
+  });
 
   function createTweetElement(tweetObj) {
     return $.parseHTML(`
@@ -68,8 +47,8 @@ function loadTweets(){
             <header class='topheader'>
                 <section class='toparea'>
                     <img src='${
-                        tweetObj.user.avatars.regular
-                        }' alt='TweeterPic' width="60" height='60'>
+                      tweetObj.user.avatars.regular
+                    }' alt='TweeterPic' width="60" height='60'>
                     <h1>${tweetObj.user.name}</h1>
                     <h4>${tweetObj.user.handle}</h4>
                 </section>  
@@ -82,18 +61,7 @@ function loadTweets(){
                     <h5>${tweetObj.created_at}</h5>
                 </section>
             </footer>
-        </article>`
-    );
+        </article>`);
   }
-  loadTweets()
-
-
-  // $('button>composebutton').click(function(){
-  //   $('article').toggle();
-  // })
-
-
-
-
+  loadTweets();
 });
-
